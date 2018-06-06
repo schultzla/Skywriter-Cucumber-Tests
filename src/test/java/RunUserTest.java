6a406c9373825da8a3628a22344dba62c21af6ed
Logan Schultz <schultzlogana@gmail.com>
2018-06-06 13:15:42 -0400

    Moved files around

---
+++ src/test/java/RunUserTest.java
@@ -0,0 +1,23 @@
+
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+        features = {"src/test/java/resources"},
+		plugin= {
+				"pretty",
+				"html:target/cucumber-html-report",
+				"json:target/cucumber-report.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport.html"
+		}
+		
+		
+		)
+
+public class RunUserTest {
+	
+}
