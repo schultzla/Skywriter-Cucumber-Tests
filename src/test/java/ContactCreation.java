8f6a6c8b6ba1e351cbf542eca387374382bda1db
Logan Schultz <schultzlogana@gmail.com>
2018-06-06 18:23:39 -0400

    I'm an idiot and forgot to commit the pom file

--- src/test/java/ContactCreation.java
+++ src/test/java/ContactCreation.java
@@ -4,8 +4,6 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
